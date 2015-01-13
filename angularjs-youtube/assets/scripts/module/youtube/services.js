e93f615fe501b1a3ff70dc1cc60f98eff4eb277f
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-01-12 23:32:41 -0300

    Update angularjs-youtube

--- angularjs-youtube/assets/scripts/module/youtube/services.js
+++ angularjs-youtube/assets/scripts/module/youtube/services.js
@@ -8,13 +8,15 @@
 
 	function YoutubeService($http) {
 		var YoutubeAPI = {
-	        userUploads: 'http://gdata.youtube.com/feeds/api/users/{username}/uploads?alt=json'
+			defaultParams: '&orderBy=published&start-index=1',
+	        userUploads: 'http://gdata.youtube.com/feeds/api/users/{username}/uploads?alt=json',
+	        singleVideo: 'https://gdata.youtube.com/feeds/api/videos/{videoid}?alt=json'
 	    };
 
 	    return {
 	    	getUserVideos: function(query) {
 	    		var url = YoutubeAPI.userUploads.replace(/(\{username\})/g, query);
-	    		return $http.get(url);
+	    		return $http.get(url + YoutubeAPI.defaultParams);
 	    	}
 	    };
 	}
